0fbba2623c2a53a8e29adbf24a57449ae701a215
VanSu99 <vansutan99@gmail.com>
2021-06-23 12:56:01 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Home from "./components/Map/Map.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Home },
+};
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 0;
+  color: #000000;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 50px 0;
 }
 </style>
---
+++ src/components/Map/Map.vue
@@ -0,0 +1,181 @@
+<template>
+  <div class="panel">
+    <div class="map-box route">
+      <div id="map"></div>
+    </div>
+    <div class="map-actions">
+      <p v-if="address">
+        Your location: <span>{{ address }}</span>
+      </p>
+      <button class="btn" @click="handleLocateCurrent">Your Location</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import mapboxgl from "mapbox-gl";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      accessToken: "pk.eyJ1IjoiYmF0Y3Jhenk3NDEiLCJhIjoiY2txNjRkMWsyMHY3eTJ4bnR1cWRkYXZqZiJ9.62M5-Fnm7f16nc1Uh_HpOQ",
+      coords: {},
+      map: null,
+      enteredAddress: "",
+      address: "",
+      center: [108.2772, 14.0583],
+      startCoord: [],
+      destiCoord: [],
+    };
+  },
+
+  mounted() {
+    this.map = this.getMap(this.center, 7, "map");
+    this.map.on("load", () => {
+      this.map.resize();
+    });
+  },
+
+  methods: {
+    // init map
+    getMap: function (center, zoom, container) {
+      //let destPoint;
+      mapboxgl.accessToken = this.accessToken;
+      if (center.length > 1) {
+        const map = new mapboxgl.Map({
+          container,
+          style: "mapbox://styles/mapbox/streets-v11",
+          center,
+          zoom,
+        });
+
+        const geocoder = new MapboxGeocoder({
+          // Initialize the geocoder
+          accessToken: mapboxgl.accessToken,
+          mapboxgl: mapboxgl,
+          marker: { color: "orange" },
+          placeholder: "Search for places in Berkeley",
+        });
+
+        map.addControl(geocoder);
+        return map;
+      }
+    },
+
+    // init user marker
+    setMapboxMarker: function (pos, map) {
+      new mapboxgl.Marker({
+        draggable: true,
+        color: "blue",
+      })
+        .setLngLat(pos)
+        .addTo(map);
+    },
+
+    getAddressFromCoordinate: async function (coords) {
+      const response = await fetch(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${coords.longitude},${coords.latitude}.json?access_token=${this.accessToken}`
+      );
+      const data = await response.json();
+      return data.features[0].place_name;
+    },
+
+    // get user current
+    handleLocateCurrent: function () {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          //location success
+          async (position) => {
+            this.coords = position.coords;
+            this.address = await this.getAddressFromCoordinate(this.coords);
+            const centerLocation = [position.coords.longitude, position.coords.latitude];
+            this.startCoord = [...centerLocation];
+            this.map = this.getMap(centerLocation, 15, "map");
+            this.setMapboxMarker(centerLocation, this.map);
+          },
+          //location error
+          (error) => {
+            alert("Không tìm thấy vị trí của bạn. Vui lòng thử lại.");
+            console.log(error);
+          }
+        );
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.marker {
+  background-color: red;
+  border: 2px solid white;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  cursor: pointer;
+}
+.pulse {
+  box-shadow: 0 0 0 0 rgba(0, 0, 0, 1);
+  animation: pulse 2s infinite;
+}
+
+@keyframes pulse {
+  0% {
+    box-shadow: 0 0 0 0 rgba(0, 0, 0, 0.7);
+  }
+
+  70% {
+    box-shadow: 0 0 0 10px rgba(0, 0, 0, 0);
+  }
+
+  100% {
+    box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
+  }
+}
+
+.panel {
+  display: flex;
+  justify-content: space-between;
+}
+
+.map-box {
+  position: relative;
+  height: 400px;
+  width: calc(60% - 30px);
+}
+
+#map {
+  height: 100%;
+  width: 100%;
+}
+
+.search {
+  position: absolute;
+  left: 20px;
+  top: 20px;
+}
+
+.search-input {
+  outline: none;
+  border: 0;
+  padding: 10px;
+}
+
+.map-actions {
+  width: calc(40% - 30px);
+}
+
+.btn {
+  outline: none;
+  cursor: pointer;
+  border: 0;
+  border-radius: 3px;
+  padding: 7px 12px;
+  background-color: #1473e6;
+  color: #fff;
+  font-size: 15px;
+  font-weight: 600;
+}
+</style>
